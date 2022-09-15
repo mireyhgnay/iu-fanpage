9e703577d09c2672c6c8aa30461146f5136b2321
hyerim <hyerimiya1216@gmail.com>
2022-09-15 12:51:31 +0900

    Updates

---
+++ precache-manifest.59c2e821399d7e9248b5542985adc02a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a449032f04dac2a05b8faf52e994a42e",
+    "url": "/iu-fanpage/index.html"
+  },
+  {
+    "revision": "9a7709994c99d17d8b0b",
+    "url": "/iu-fanpage/static/css/main.23dca78f.chunk.css"
+  },
+  {
+    "revision": "676c51dc5914ce6e55f2",
+    "url": "/iu-fanpage/static/js/2.e9af0f79.chunk.js"
+  },
+  {
+    "revision": "4472b9b1c403804b7335229d4dbf4deb",
+    "url": "/iu-fanpage/static/js/2.e9af0f79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a7709994c99d17d8b0b",
+    "url": "/iu-fanpage/static/js/main.57689f8d.chunk.js"
+  },
+  {
+    "revision": "deb4399653367589cf27",
+    "url": "/iu-fanpage/static/js/runtime-main.e2bafd1a.js"
+  }
+]);
